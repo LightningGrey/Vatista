525303caed07dec9150233b2133963ffa6330905
gary <gary.liu@ontariotechU.net>
2020-01-20 17:02:17 -0500

    testing commit
    test commit

--- src/UI.cpp
+++ src/UI.cpp
@@ -18,26 +18,26 @@ int StaminaBar::getTexture() const {
 
 
 //code snipit example of cascading shader, will use for stamina bar
-//vec3 hsv2rgb(vec3 c)
-//{
-//    vec4 K = vec4(1.0, 0.6666666, 0.3333333, 3.0);
-//    vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-//    return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+//vec3 testing(vec3 c)
+//{//xyzw parameters for vec 4 colours xyz = rgb w = alpha
+//	vec4 K = vec4(1.0, 1.0, 0.0, 0.0);
+//	vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+//	return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 //}
 //
 //
 //void mainImage(out vec4 fragColor, in vec2 fragCoord)
 //{
-//    vec2 border = vec2(0.05, 0.1);
-//    vec4 backCol = vec4(1, 1, 1, 1);
-//    vec2 uv = fragCoord / iResolution.xy;
+//	vec2 border = vec2(0.05, 0.1);
+//	vec4 backgroundColor = vec4(1, 1, 1, 1);//background color
+//	vec2 uv = fragCoord / iResolution.xy;//fragment shader cordinates
 //
-//    // generate border mask
-//    vec2 mask2 = step(border, uv) * step(uv, 1.0 - border);
-//    float mask = mask2.x * mask2.y;
+//	// generate border mask
+//	vec2 mask2 = step(border, uv) * step(uv, 1.0 - border);
+//	float mask = mask2.x * mask2.y;
 //
-//    float progress = mod(iTime, 1.0);
-//    float blend = ((uv.x - progress) <= 0.0 ? 1.0 : 0.0) * mask;
-//    vec4 foreCol = vec4(hsv2rgb(vec3(progress * 0.33333 - 0.1, 1.0, 1.0)), 1.0);
-//    fragColor = foreCol * blend + backCol * (1.0 - blend);
+//	float progress = mod(iTime, 1.0); //changes the colour based on time 
+//	float blend = ((uv.x - progress) <= 0.0 ? 1.0 : 0.0) * mask;
+//	vec4 foreCol = vec4(testing(vec3(progress * 0.33333 - 0.1, 1.0, 1.0)), 1.0);
+//	fragColor = foreCol * blend + backgroundColor * (0.9 - blend);//background
 //}
