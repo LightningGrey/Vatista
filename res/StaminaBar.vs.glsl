4e5365a91cc72097fc7b4d44662b2ffeb9d83322
gary <gary.liu@ontariotechU.net>
2020-02-05 18:45:43 -0500

    stamina finally working

--- res/StaminaBar.vs.glsl
+++ res/StaminaBar.vs.glsl
@@ -3,11 +3,13 @@
 layout (location = 0) in vec3 inPosition;
 layout (location = 1) in vec2 inUV;
 layout (location = 2) in vec3 inNormal;
+//layout (location = 3) in vec3 incolor;
 
 
 layout (location = 0) out vec3 outPosition;
 layout (location = 1) out vec2 outUV;
 layout (location = 2) out vec3 outNormal;
+//layout (location = 3) out vec3 outcolor;
 
 
 
@@ -19,16 +21,14 @@ uniform mat4 a_Model;
 uniform mat4 a_ModelView;
 uniform mat3 a_NormalMatrix;
 uniform float staminaValue;
-uniform vec3 vecVert;
 uniform float staminaValueMax;
-
+uniform vec3 meshDimensions;
 	
 void main() {
 	outPosition = inPosition;
 	outUV = inUV;
 	outNormal = a_NormalMatrix * inNormal;
 
-gl_Position = a_ModelViewProjection * vec4(inPosition, 1);
 	
 //	outPosition =  (a_Model * vec4(inPosition, 1)).xyz;
 
@@ -44,9 +44,13 @@ gl_Position = a_ModelViewProjection * vec4(inPosition, 1);
 	// = setScale(glm::vec3(1.0f))
 //gl_Position = vec4(outPosition, 1.0f);
 
+//
+//vec3 temp = meshDimensions;
+//temp.x = meshDimensions.x * (staminaValue/staminaValueMax);
+//outPosition = temp;
 
-vec3 temp = vecVert;
-temp.x = vecVert.x * (staminaValue/staminaValueMax);
-outPosition = temp;
+//gl_Position = a_ModelViewProjection * vec4(temp, 1);
+gl_Position = a_ModelViewProjection *  vec4(outPosition, 1);
+
 }
 
