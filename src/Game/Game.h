cfb57587a5d01a2225c2b5d922dd69baa3116bf3
Jiminy Cao <jiminy.cao@gmail.com>
2020-03-08 22:51:58 -0400

    toggles and camera

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -77,9 +77,9 @@ namespace Vatista {
 		StationaryObj::Sptr stage;
 		float x = 100.0f;
 		float y = 100.0f;
-
+		float move = 0.1f;
 		//GameObject::Sptr TestStaminaBackground;
-
+		std::vector<bool> toggles{0,0,0,0,0,0,0,0,0,0};
 	
 		Light::Sptr point;
 		StationaryObj::Sptr prop;
