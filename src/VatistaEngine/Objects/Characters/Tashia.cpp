606d6c83b29ca5bbc927deb9cba5b60495b53715
Jiminy Cao <jiminy.cao@gmail.com>
2020-03-30 23:40:14 -0400

    [Added] Tashia character class (just the skeleton)

---
+++ src/VatistaEngine/Objects/Characters/Tashia.cpp
@@ -0,0 +1,15 @@
+#include "Tashia.h"
+ 
+namespace Vatista {
+	Tashia::Tashia(bool ID, std::vector<Mesh::Sptr>& meshes, Material::Sptr mat)
+	{
+	}
+
+	Tashia::~Tashia()
+	{
+	}
+
+	void Tashia::update(float dt, GLFWwindow* gameWindow, Character::Sptr p2, AudioEngine::Sptr ae)
+	{
+	}
+}
